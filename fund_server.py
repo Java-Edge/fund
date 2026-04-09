513a9f8a94128de2be4d4796c26fd5df8316ea05
JavaEdge <sss2855845850@gmail.com>
2026-04-10 00:03:09 +0800

    Update fund_server.py
    Signed-off-by: JavaEdge <sss2855845850@gmail.com>

--- fund_server.py
+++ fund_server.py
@@ -4,10 +4,8 @@ from app.factory import create_app as build_app
 
 app = build_app()
 
-
 def create_app() -> Flask:
     return app
 
-# 只保留入口职责
 if __name__ == "__main__":
     create_app().run(host="localhost", port=8311)
